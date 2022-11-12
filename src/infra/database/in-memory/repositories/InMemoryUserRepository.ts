d0efb6762373e3ce18519b106f64a01b6f6c13ee
deal-machine <95deal@gmail.com>
2022-11-11 21:56:11 -0300

    feat: add id to user entity

---
+++ src/infra/database/in-memory/repositories/InMemoryUserRepository.ts
@@ -0,0 +1,43 @@
+import { User } from "src/domain/entities/user";
+import { Encrypter } from "src/infra/protocols/encrypter";
+import { IdGenerator } from "src/infra/protocols/id-generator";
+import { UserCreateDTO } from "src/usecases/modules/createUser/create-user";
+import { UserRepository } from "src/usecases/protocols/user-repository";
+
+export class UserRepositoryInMemory implements UserRepository {
+    private readonly userEntity: User[];
+    private readonly idGenerator: IdGenerator;
+    private readonly encrypter: Encrypter;
+
+    constructor(IdGenerator: IdGenerator, encrypter: Encrypter) {
+        this.userEntity = [];
+        this.idGenerator = IdGenerator;
+        this.encrypter = encrypter;
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userEntity;
+    }
+
+    async findByEmail(email: string): Promise<User | undefined> {
+        return this.userEntity.find((user) => user.email === email);
+    }
+
+    async create(user: UserCreateDTO): Promise<User> {
+        const newUser: User = {
+            id: this.idGenerator.random(),
+            email: user.email,
+            name: user.name.toUpperCase(),
+            password: await this.encrypter.hash(user.password),
+        };
+
+        this.userEntity.push(newUser);
+
+        return newUser;
+    }
+
+    async exists(email: string): Promise<boolean> {
+        const userExists = this.userEntity.find((user) => user.email === email);
+        return !!userExists;
+    }
+}
--- src/usecases/protocols/user-repository.ts
+++ src/usecases/protocols/user-repository.ts
@@ -1,8 +1,9 @@
-import { UserData } from "src/domain/entities/user";
+import { User } from "src/domain/entities/user";
+import { UserCreateDTO } from "../modules/createUser/create-user";
 
 export interface UserRepository {
-    findAll: () => Promise<UserData[]>;
-    findByEmail: (email: string) => Promise<UserData>;
-    create: (user: UserData) => Promise<void>;
+    findAll: () => Promise<User[]>;
+    findByEmail: (email: string) => Promise<User | undefined>;
+    create: (user: UserCreateDTO) => Promise<User>;
     exists: (email: string) => Promise<boolean>;
 }
