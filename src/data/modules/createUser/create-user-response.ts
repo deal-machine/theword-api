bfc784b37813b8dea45a9cf80c61f92631094f93
deal-machine <95deal@gmail.com>
2022-11-12 13:33:45 -0300

    feat: apply encrypter password and idGenerator in  usecase

--- src/data/modules/createUser/create-user-response.ts
+++ src/data/modules/createUser/create-user-response.ts
@@ -2,4 +2,4 @@ import { User } from "@entities/user";
 import { InvalidEmail } from "@data/errors/invalid-email";
 import { InvalidPassword } from "@data/errors/invalid-password";
 
-export type CreateUserResponse = InvalidEmail | InvalidPassword | User;
+export type CreateUserResponse = User;
--- src/data/modules/createUser/create-user-usecase.ts
+++ src/data/modules/createUser/create-user-usecase.ts
@@ -1,19 +1,31 @@
 import { UserRepository } from "@data/protocols/user-repository";
-import {
-    CreateUser,
-    UserCreateDTO,
-} from "../../../domain/usecases/create-user";
+import { Encrypter } from "@data/protocols/encrypter";
+import { IdGenerator } from "@data/protocols/id-generator";
+import { CreateUser, UserCreateDTO } from "@usecases/create-user";
 import { CreateUserResponse } from "./create-user-response";
 
 export class CreateUserUseCase implements CreateUser {
     private readonly userRepository: UserRepository;
+    private readonly idGenerator: IdGenerator;
+    private readonly encrypter: Encrypter;
 
-    constructor(userRepo: UserRepository) {
+    constructor(
+        userRepo: UserRepository,
+        encrypter: Encrypter,
+        idGenerator: IdGenerator
+    ) {
         this.userRepository = userRepo;
+        this.encrypter = encrypter;
+        this.idGenerator = idGenerator;
     }
 
     async createUser(user: UserCreateDTO): Promise<CreateUserResponse> {
-        const newUser = await this.userRepository.create(user);
+        const newUser = await this.userRepository.create({
+            id: this.idGenerator.random(),
+            email: user.email.toUpperCase(),
+            name: user.name.toUpperCase(),
+            password: await this.encrypter.hash(user.password),
+        });
         return newUser;
     }
 }
