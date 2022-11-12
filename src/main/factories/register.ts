c30e382c4dd5eb56a06c9006c254d2ec082e087c
deal-machine <95deal@gmail.com>
2022-11-11 21:57:55 -0300

    feat: register dependencies instances

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,7 +1,13 @@
-// instancia models
-// instancia repositories
-// instancia casos de uso
-// instancia controller
+import { UserRepositoryInMemory } from "src/infra/database/in-memory/repositories";
+import { Bcrypt } from "src/infra/utils/bcrypt/bcrypt";
+import { Uuid } from "src/infra/utils/id-generator/uuid";
+import { CreateUserController } from "src/presentation/controllers/create-user-controller";
+import { CreateUserUseCase } from "src/usecases/modules/createUser/create-user-usecase";
 
-// injeta as dependencias
-// exporta a controller criada
+export const makeCreateUserController = (): CreateUserController => {
+    const idGenerator = new Uuid();
+    const encrypter = new Bcrypt();
+    const userRepository = new UserRepositoryInMemory(idGenerator, encrypter);
+    const createUserUseCase = new CreateUserUseCase(userRepository);
+    return new CreateUserController(createUserUseCase);
+};
