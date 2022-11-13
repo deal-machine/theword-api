98c8cf5dae11f42f2558a2db422ca71d09f03d1f
deal-machine <95deal@gmail.com>
2022-11-12 23:50:34 -0300

    feat: generate User model and static method to create

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,6 +1,23 @@
-export interface User {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
+export class User {
+    public readonly id?: string;
+    public readonly name: string;
+    public readonly email: string;
+    public readonly password: string;
+
+    private constructor({ id, name, password, email }: User) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        Object.freeze(this);
+    }
+
+    static create(userDTO: User) {
+        return new User({
+            id: userDTO.id,
+            email: userDTO.email.toUpperCase(),
+            name: userDTO.name.toUpperCase(),
+            password: userDTO.password,
+        });
+    }
 }
